b0595a595d92b85781d759572856a96178aeef37
alexei dorofeev <dorofeev86@yahoo.com>
2020-11-15 12:29:54 +0100

    installed next.js with chakra

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  Icon,
+  List,
+  ListIcon,
+  ListItem,
+} from "@chakra-ui/core";
+
+const Index = () => <div>Hello World</div>;
+
+export default Index;
