0af5bedc673fa2cc3acf959b2696f3fd7947a867
alexei <dorofeev86@yahoo.com>
2020-11-15 22:09:50 +0100

    install URQL

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -10,14 +10,32 @@ import {
 } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      username
+    }
+  }
+}
+`;
+
 export const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
   return (
     <Formik
       initialValues={{ username: "", password: "" }}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={async (values) => {
+        const response = await register(values);
+      }}
     >
       {({ isSubmitting }) => (
         <Wrapper variant="small">
