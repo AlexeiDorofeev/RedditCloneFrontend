df7647ffd325dce2ce6753dbe3922162b0c0ff6b
alexei <dorofeev86@yahoo.com>
2020-11-22 11:28:27 +0100

    change password

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form, Formik } from "formik";
-import { Box, Button } from "@chakra-ui/core";
+import { Box, Button, Flex, Link } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
 import { useLoginMutation } from "../generated/graphql";
@@ -8,6 +8,7 @@ import { toErrorMap } from "../utils/toErrorMap";
 import { useRouter } from "next/router";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
+import NextLink from "next/link";
 
 interface loginProps {}
 
@@ -15,19 +16,19 @@ export const Login: React.FC<loginProps> = ({}) => {
   const router = useRouter();
   const [, login] = useLoginMutation();
   return (
-    <Formik
-      initialValues={{ usernameOrEmail: "", password: "" }}
-      onSubmit={async (values, { setErrors }) => {
-        const response = await login(values);
-        if (response.data?.login.errors) {
-          setErrors(toErrorMap(response.data.login.errors));
-        } else if (response.data?.login.user) {
-          router.push("/");
-        }
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Wrapper variant="small">
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ usernameOrEmail: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login(values);
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
           <Form>
             <InputField
               name="usernameOrEmail"
@@ -42,6 +43,11 @@ export const Login: React.FC<loginProps> = ({}) => {
                 type="password"
               />
             </Box>
+            <Flex mt={2}>
+              <NextLink href="/forgot-password">
+                <Link ml="auto">forgot password?</Link>
+              </NextLink>
+            </Flex>
             <Button
               mt={4}
               isLoading={isSubmitting}
@@ -51,9 +57,9 @@ export const Login: React.FC<loginProps> = ({}) => {
               login
             </Button>
           </Form>
-        </Wrapper>
-      )}
-    </Formik>
+        )}
+      </Formik>
+    </Wrapper>
   );
 };
 
